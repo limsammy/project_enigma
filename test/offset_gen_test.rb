e0562c91ae00e9111b50f6fc7ce5acb296abce11
Sam Lim <limsammy1@gmail.com>
2017-03-23 19:18:07 -0600

    got offset_gen tests passing

---
+++ test/offset_gen_test.rb
@@ -0,0 +1,32 @@
+gem 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/offset_gen'
+
+class OffsetGenTest < Minitest::Test
+  def test_object_is_created
+    gen = OffsetGen.new
+
+    assert_instance_of OffsetGen, gen
+  end
+
+  def test_object_is_created_with_default_length_4
+    gen = OffsetGen.new
+
+    assert_equal 4, gen.length
+  end
+
+  def test_length_can_be_changed_to_something_other_than_5
+    gen = OffsetGen.new
+    gen.length = 2
+
+    assert_equal 2, gen.length
+  end
+
+  def test_generate_offset_method_generates_offset_default_length_5
+    gen = OffsetGen.new
+    offset = gen.generate_offset
+
+    assert_equal 4, offset.to_s.length
+  end
+end
