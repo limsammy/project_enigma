c390eed38ef04aea2aba5b047be0a5c189be3d4d
Sam Lim <limsammy1@gmail.com>
2017-03-23 20:15:33 -0600

    rotation calculator tests pass

--- test/cipher_builder_test.rb
+++ test/cipher_builder_test.rb
@@ -9,4 +9,32 @@ class CipherBuilderTest < Minitest::Test
 
     assert_instance_of Array, builder.char_map
   end
+
+  def test_rotation_a_is_created
+    b = CipherBuilder.new
+    a_rotation = b.calculate_rotation('A', '03165', '0489')
+
+    assert_equal 3, a_rotation
+  end
+
+  def test_rotation_b_is_created
+    b = CipherBuilder.new
+    b_rotation = b.calculate_rotation('B', '03165', '0489')
+
+    assert_equal 35, b_rotation
+  end
+
+  def test_rotation_c_is_created
+    b = CipherBuilder.new
+    c_rotation = b.calculate_rotation('C', '03165', '0489')
+
+    assert_equal 24, c_rotation
+  end
+
+  def test_rotation_d_is_created
+    b = CipherBuilder.new
+    d_rotation = b.calculate_rotation('D', '03165', '0489')
+
+    assert_equal 74, d_rotation
+  end
 end
