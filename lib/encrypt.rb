308a4f9d18e4a22c9eef97be54757e6a39d7b1e2
Sam Lim <limsammy1@gmail.com>
2017-03-23 21:03:54 -0600

    added some pseudocode, generated cipher tables

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,5 +1,6 @@
 require_relative 'key_gen'
 require_relative 'offset_gen'
+require_relative 'cipher_builder'
 
 class Encrypt
 
@@ -7,13 +8,20 @@ class Encrypt
 
   def initialize
     # assign random 5 digits to instance variable @key
-    @key = KeyGen.new.generate_key(5)
+    @key      = KeyGen.new.generate_key(5)
     # assign todays date in ddmmyy format as default offset
-    @offset = OffsetGen.new.generate_offset(4)
+    @offset   = OffsetGen.new.generate_offset(4)
+    # generate cipher tables as instance variables
+    @a_cipher = CipherBuilder.new.create_table('A', @key.to_s, @offset.to_s)
+    @b_cipher = CipherBuilder.new.create_table('B', @key.to_s, @offset.to_s)
+    @c_cipher = CipherBuilder.new.create_table('C', @key.to_s, @offset.to_s)
+    @d_cipher = CipherBuilder.new.create_table('D', @key.to_s, @offset.to_s)
   end
 
   def encrypt(message, key=@key, offset=@offset)
-    char_map = map
+    char_map = gen_map
+    # rotate A indices first, then B, etc.
+    # Brainstorm way to check if index is ABC or D
   end
 
   def gen_map
