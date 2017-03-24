16c7d4ba3815c18a95f4930d3f22687799204088
Sam Lim <limsammy1@gmail.com>
2017-03-23 19:24:35 -0600

    object_is_class_type and key length tests passing

--- test/encrypt_test.rb
+++ test/encrypt_test.rb
@@ -1,4 +1,18 @@
 gem 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
-require './lib/pattern_generator'
+require './lib/encrypt'
+
+class EncryptTest < Minitest::Test
+  def test_object_is_class_type
+    e = Encrypt.new
+
+    assert_instance_of Encrypt, e
+  end
+
+  def test_object_is_created_with_default_key_length_5
+    e = Encrypt.new
+
+    assert_equal 5, e.key.to_s.length
+  end
+end
