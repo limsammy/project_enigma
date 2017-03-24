228dda33675d757b59f2a5a20b9d16a7cc0db0e8
Sam Lim <limsammy1@gmail.com>
2017-03-23 20:15:54 -0600

    wrote calculate_rotation method

--- lib/cipher_builder.rb
+++ lib/cipher_builder.rb
@@ -6,6 +6,18 @@ class CipherBuilder
   def initialize
     @char_map = Encrypt.new.gen_map
   end
-  # create class that builds HASH cipher tables for each ABCD rotation
-  # create method(or class?) to calculate rotation
+  # rotation calculator
+  def calculate_rotation(rotation_id, key, offset)
+    if rotation_id == 'A'
+      a_rotation = key[0..1].to_i + offset[0].to_i
+    elsif rotation_id == 'B'
+      b_rotation = key[1..2].to_i + offset[1].to_i
+    elsif rotation_id == 'C'
+      c_rotation = key[2..3].to_i + offset[2].to_i
+    elsif rotation_id == 'D'
+      d_rotation_id = key[3..4].to_i + offset[3].to_i
+    else
+      puts "No rotation of type #{rotation} exists."
+    end
+  end
 end
