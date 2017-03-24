dc9ce11c99a20c2b090058db6863a4b4b17c8bbb
Sam Lim <limsammy1@gmail.com>
2017-03-23 20:28:26 -0600

    slight refactor

--- lib/cipher_builder.rb
+++ lib/cipher_builder.rb
@@ -1,4 +1,5 @@
 require_relative 'encrypt'
+require 'pry'
 
 class CipherBuilder
   attr_reader :char_map
@@ -6,18 +7,23 @@ class CipherBuilder
   def initialize
     @char_map = Encrypt.new.gen_map
   end
-  # rotation calculator
+
   def calculate_rotation(rotation_id, key, offset)
     if rotation_id == 'A'
-      a_rotation = key[0..1].to_i + offset[0].to_i
+      key[0..1].to_i + offset[0].to_i
     elsif rotation_id == 'B'
-      b_rotation = key[1..2].to_i + offset[1].to_i
+      key[1..2].to_i + offset[1].to_i
     elsif rotation_id == 'C'
-      c_rotation = key[2..3].to_i + offset[2].to_i
+      key[2..3].to_i + offset[2].to_i
     elsif rotation_id == 'D'
-      d_rotation_id = key[3..4].to_i + offset[3].to_i
+      key[3..4].to_i + offset[3].to_i
     else
       puts "No rotation of type #{rotation} exists."
     end
+  end
+
+  def create_table
+    table = Hash[@char_map.map { |x| [x,1] }]
+    binding.pry
   end
 end
